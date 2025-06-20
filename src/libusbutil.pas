8e6015a3a9ac13da58be4d7cdeae747d027c69fa
Johann Glaser <Johann.Glaser@gmx.at>
2025-06-20 18:45:17 +0200

    Added TLibUsbDeviceIntfInfo

--- src/libusbutil.pas
+++ src/libusbutil.pas
@@ -64,6 +64,41 @@ Function MatchEPIntrIn (EP:Plibusb_endpoint_descriptor;Data:Pointer) : Boolean;
 Function MatchEPIntrOut(EP:Plibusb_endpoint_descriptor;Data:Pointer) : Boolean;
 
 Type
+
+  { TLibUsbDeviceIntfInfo }
+
+  TLibUsbDeviceIntfInfo = class
+  public
+    // values from create
+    FContext : TLibUsbContext;
+    FVid     : Word;
+    FPid     : Word;
+    FSerial  : String;
+    FConfig  : Byte;
+    FIntf    : Byte;
+    // values determined by Find
+    FBusNumber  : Byte;
+    FPortNumber : Byte;
+    FPortPath   : TDynByteArray;
+    FAddress    : Byte;
+    FVendor     : String;
+    FProduct    : String;
+    FSerialDesc : String;
+{$IFDEF LINUX}
+    FSysDev     : String;
+    FSysDevDir  : String;
+    FSysIntf    : String;
+    FSysIntfDir : String;
+    FSysTty     : String;
+    FDevTty     : String;
+{$ENDIF}   // LINUX
+    FDev        : Plibusb_device;
+  public
+    Constructor Create(AContext:TLibUsbContext;AVid,APid:Word;ASerial:String;AConfig,AIntf:Byte);
+    Function Find : Plibusb_device;
+    Function HasTty : String;
+  End;
+
   { TLibUsbDeviceWithFirmware }
 
   TLibUsbDeviceWithFirmware = class(TLibUsbDevice)
@@ -142,6 +177,127 @@ Function MatchEPIntrOut(EP:Plibusb_endpoint_descriptor;Data:Pointer) : Boolean;
 Begin
   Result := (EP^.bEndpointAddress and LIBUSB_ENDPOINT_DIR_MASK  = LIBUSB_ENDPOINT_OUT) and
             (EP^.bmAttributes     and LIBUSB_TRANSFER_TYPE_MASK = LIBUSB_TRANSFER_TYPE_INTERRUPT);
+End;
+
+{ TLibUsbDeviceIntfInfo }
+
+Constructor TLibUsbDeviceIntfInfo.Create(AContext : TLibUsbContext; AVid, APid : Word; ASerial : String; AConfig, AIntf : Byte);
+Begin
+  inherited Create;
+  FContext := AContext;
+  FPid     := APid;
+  FVid     := AVid;
+  FSerial  := ASerial;
+  FConfig  := AConfig;
+  FIntf    := AIntf;
+End;
+
+Function TLibUsbDeviceIntfInfo.Find : Plibusb_device;
+Var Devs     : TLibUsbDeviceArray;
+    DIdx     : Integer;
+    Matcher  : TLibUsbDeviceMatchClass;
+{$IFDEF LINUX}
+    I        : Integer;
+{$ENDIF}
+Begin
+  Result := Nil;
+  SetLength(Devs,0);
+  Matcher := TLibUsbDeviceMatchVidPid.Create(FContext, FVid, FPid);
+  // find configured devices, but don't complain if non were found
+  Devs := FContext.FindDevices(Matcher,false,0);
+  Matcher.Free;
+  WriteLn('Found ',Length(Devs),' devices:');
+  For DIdx := 0 to Length(Devs)-1 do
+    Begin
+      FBusNumber  := FContext.GetBusNumber(Devs[DIdx]);
+      FPortNumber := FContext.GetPortNumber(Devs[DIdx]);
+      FPortPath   := FContext.GetPortPath(Devs[DIdx]);
+      FAddress    := FContext.GetDeviceAddress(Devs[DIdx]);
+      WriteLn('Bus Number: ', FBusNumber);
+      WriteLn('Port Number: ', FPortNumber);
+      Write('Port Path: ');
+      For I := 0 to Length(FPortPath)-1 do
+        Write(FPortPath[I],' ');
+      WriteLn;
+      WriteLn('Device Address: ', FAddress);
+{$IFDEF LINUX}
+      // create filename used in /sys for the USB device
+      FSysDev := IntToStr(FBusNumber)+'-';
+      For I := 1 to Length(FPortPath)-1 do
+        FSysDev := FSysDev + IntToStr(FPortPath[I]) + '.';
+      FSysDev := FSysDev + IntToStr(FPortNumber);
+      WriteLn(FSysDev);
+      FSysDevDir := '/sys/bus/usb/devices/'+FSysDev;
+      if not DirectoryExists(FSysDevDir) then
+        continue;
+      FVendor  := Trim(GetFileAsString(FSysDevDir+'/manufacturer'));
+      FProduct := Trim(GetFileAsString(FSysDevDir+'/product'));
+      WriteLn('Vendor: ', FVendor);
+      WriteLn('Product: ', FProduct);
+      if FileExists(FSysDevDir+'/serial') then
+        FSerialDesc := Trim(GetFileAsString(FSysDevDir+'/serial'));
+      WriteLn('Serial = ',FSerialDesc, ' =?= ', FSerial);
+      // check serial number if required
+      if (FSerial > '') and (FSerialDesc <> FSerial) then
+        continue;
+      // create filename for USB interface
+      FSysIntf := FSysDev+':'+IntToStr(FConfig)+'.'+IntToStr(FIntf);
+      FSysIntfDir := FSysDevDir+'/'+ FSysIntf;
+      WriteLn('/sys Dir = ',FSysIntfDir);
+      if DirectoryExists(FSysIntfDir) then
+        Begin
+          WriteLn('FOUND!');
+          FDev   := Devs[DIdx];
+          Result := FDev;
+          // the values in this object are only valid, if the return-value is non-Nil
+          Exit;
+        End;
+{$ENDIF}
+    End;
+End;
+
+Function TLibUsbDeviceIntfInfo.HasTty : String;
+Var
+{$IFDEF LINUX}
+    SRec       : TSearchRec;
+{$ENDIF}
+Begin
+  Result := '';
+{$IFDEF LINUX}
+  // check if Find was already called
+  if FSysIntfDir = '' then
+    raise Exception.Create('Call Find before HasTty');
+
+  if not DirectoryExists(FSysIntfDir+'/tty') then
+    Exit;
+  // search for filename of the TTY device
+  FSysTty := '';
+  FindFirst(FSysIntfDir+'/tty/*', faDirectory, SRec);
+  try
+    repeat
+      WriteLn('Found ',SRec.Name);
+      if SRec.Name[1] = '.' then
+        continue;
+      if FSysTty > '' then
+        Begin
+          WriteLn('Strange, FSysTty is already set');
+          continue;
+        End;
+      FSysTty := SRec.Name;
+    until FindNext(SRec) <> 0;
+  finally
+    FindClose(SRec);
+  End;
+  if FSysTty = '' then
+    Exit;
+  FDevTty := '/dev/'+FSysTty;
+  if not FileExists(FDevTty) then
+    Begin
+      WriteLn('Strange, ',FDevTty,' doesn''t exist.');
+      Exit;
+    End;
+  Result := FDevTty;
+{$ENDIF}
 End;
 
 { TLibUsbDeviceWithFirmware }
