ee0251e81f6a3e79446ab7ba43c554d50cfae5df
scharinger <scharinger@mikka.com>
2026-01-05 09:28:16 +0100

    cosmetic changes

--- src/libusb.pas
+++ src/libusb.pas
@@ -53,7 +53,7 @@ Interface
 Uses CTypes;
 
 {$ifdef windows}
-  const dllname = 'libusb-1.0.dll'; // filename of DLL
+  const dllname = 'libusb-1.0.dll'; // filename of your libusb DLL, more information here: https://github.com/libusb/libusb
 {$else}
   {$LINKLIB c}
   {$LINKLIB usb-1.0}
